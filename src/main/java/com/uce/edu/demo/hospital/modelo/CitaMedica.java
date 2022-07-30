cb819e781b1155d5330e97d34547dabc3316ca48
AndBenvds404 <andres_benavides17@hotmail.com>
2022-07-29 20:07:47 -0600

    prueba unidad2

---
+++ src/main/java/com/uce/edu/demo/hospital/modelo/CitaMedica.java
@@ -0,0 +1,11 @@
+package com.uce.edu.demo.hospital.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cita_medica")
+public class CitaMedica {
+	
+
+}
---
+++ src/main/java/com/uce/edu/demo/hospital/modelo/Paciente.java
@@ -0,0 +1,10 @@
+package com.uce.edu.demo.hospital.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "paciente")
+public class Paciente {
+
+}
